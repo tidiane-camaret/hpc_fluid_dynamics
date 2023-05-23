f5be6194ee28be2db7736a91a5e2eb51761fed38
Tidiane Camaret <camaret.tidiane@gmail.com>
2023-05-23 15:01:20 +0200

    added milestone 2 as py script

---
+++ hpc_fluid_dynamics/utils.py
@@ -0,0 +1,138 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+W, L = 200, 200 # width and length of the grid
+velocity_set = np.array([[0, 0], [0, 1], [1, 0], [0, -1], [-1, 0],
+                            [1, 1], [-1, -1], [1, -1], [-1, 1]])
+
+def init_pdf(mode = "random_uniform"): 
+    """
+    Initialize the probability distribution function (pdf) of the fluid
+    particles. The pdf is a 3D array of shape (len(velocity_set), L, W), where
+    L and W are the length and width of the grid, respectively.
+    We need to avoid zones of zero density, otherwise the simulation will
+    crash. 
+    """
+
+    pdf = np.ones((len(velocity_set), L, W)) / len(velocity_set)
+    if mode == "random_uniform":
+        for i in range(len(velocity_set)):
+            pdf[i] += np.random.uniform(0, 1, (L, W))
+
+    elif mode == "line":
+        pdf[:, :, W//2] += 0.5
+
+    elif mode == "circle":
+        for i in range(len(velocity_set)):
+            for x in range(L):
+                for y in range(W):
+                    if (x - L//2)**2 + (y - W//2)**2 < 10**2:
+                        pdf[i, x, y] += 0.5
+
+    elif mode == "square":
+        for i in range(len(velocity_set)):
+            for x in range(L):
+                for y in range(W):
+                    if abs(x - L//2) < 10 and abs(y - W//2) < 10:
+                        pdf[i, x, y] += 0.001
+
+    elif mode == "zero channel":
+        for x in range(L):
+            for y in range(W):
+                if abs(x - L//2) < 10 and abs(y - W//2) < 10:
+                    pdf[0, x, y] += 0.5
+
+    else:
+        raise ValueError("Invalid mode")
+    pdf /= np.sum(pdf)
+    return pdf
+
+def calc_density(pdf):
+    """
+    Calculate the density of the fluid particles. The density is a 2D array of
+    shape (L, W), where L and W are the length and width of the grid,
+    respectively.
+    """
+    density = np.sum(pdf, axis=0)
+    return density
+
+def calc_velocity(pdf):
+    """
+    Calculate the velocity of the fluid particles. The velocity is a 3D array
+    of shape (L, W, 2), where L and W are the length and width of the grid,
+    respectively. The third dimension is the velocity in the x and y
+    directions.
+    """
+    velocity = np.zeros((L, W, 2))
+    # loop through all positions
+    for i in range(L):
+        for j in range(W):
+            # loop through all velocities
+            for k in range(len(velocity_set)):
+                velocity[i, j, 0] += pdf[k, i, j] * velocity_set[k, 0]
+                velocity[i, j, 1] += pdf[k, i, j] * velocity_set[k, 1]
+            velocity[i, j, 0] /= calc_density(pdf)[i, j]
+            velocity[i, j, 1] /= calc_density(pdf)[i, j]
+    return velocity
+
+def calc_local_avg_velocity(pdf):
+    """
+    Calculate the local average velocity of the fluid particles. The local
+    average velocity is a 3D array of shape (L, W, 2), where L and W are the
+    length and width of the grid, respectively. The third dimension is the
+    velocity in the x and y directions.
+    """
+    density = calc_density(pdf)
+    sum = np.zeros((L, W, 2))
+    for i in range(len(velocity_set)):
+        sum[:, :, 0] += velocity_set[i, 0, None]*pdf[i, :, :]
+        sum[:, :, 1] += velocity_set[i, 1, None]*pdf[i, :, :]
+    return sum / density[:, :, None] 
+
+
+def calc_equilibrium_pdf(density, velocity):
+    """
+    Calculate the equilibrium pdf of the fluid particles. The equilibrium pdf
+    is a 3D array of shape (len(velocity_set), L, W), where L and W are the
+    length and width of the grid, respectively.
+    """
+    w = np.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
+    equilibrium_pdf = np.zeros((len(velocity_set), L, W))
+    for i in range(len(velocity_set)):
+        equilibrium_pdf[i] = w[i]*density*\
+            (1 + 
+             3*(velocity_set[i, 0]*velocity[:, :, 0] + velocity_set[i, 1]*velocity[:, :, 1]) +
+             9/2*(velocity_set[i, 0]*velocity[:, :, 0] + velocity_set[i, 1]*velocity[:, :, 1])**2 - 
+             3/2*(velocity[:, :, 0]**2 + velocity[:, :, 1]**2))
+    return equilibrium_pdf
+
+
+def streaming(pdf):
+    """ 
+    Streaming step of the Lattice Boltzmann Method (LBM). The streaming step
+    moves the fluid particles according to their velocities. The pdf is a 3D
+    array of shape (len(velocity_set), L, W), where L and W are the length and
+    width of the grid, respectively.
+    """
+    pdf_t1 = np.zeros_like(pdf)
+    # use np.roll to shift the pdf
+    for i in range(len(velocity_set)):
+        pdf_t1[i] = np.roll(pdf[i], velocity_set[i, 0], axis=0)
+        pdf_t1[i] = np.roll(pdf_t1[i], velocity_set[i, 1], axis=1)
+    return pdf_t1
+
+
+# visualize the pdf
+def plot_pdf(pdf):
+    plt.figure()
+    plt.imshow(calc_density(pdf))
+    plt.show()
+
+# visualize the velocity
+def plot_velocity(pdf):
+    plt.figure()
+    velocity = calc_velocity(pdf)
+    print(velocity.shape)
+    plt.streamplot(np.arange(W), np.arange(L), velocity[:, :, 0], velocity[:, :, 1])
+    plt.show()
---
+++ scripts/milestone2.py
@@ -0,0 +1,67 @@
+"""
+Streaming and collision step
+============================
+The streaming step is the first step of the Lattice Boltzmann Method (LBM).
+The streaming step is a simple shift of the pdf in the direction of the
+velocity. The collision step is the second step of the LBM. The collision step
+is a relaxation of the pdf towards the equilibrium pdf. The equilibrium pdf is
+the pdf that would be obtained if the fluid particles were in equilibrium. The
+equilibrium pdf is calculated using the density and the velocity of the fluid
+particles. 
+============================
+"""
+
+from matplotlib import animation
+from hpc_fluid_dynamics.utils import *
+
+pdf = init_pdf(mode="circle")
+
+fig = plt.figure()
+ax = plt.axes(xlim=(0, W), ylim=(0, L))
+im = ax.imshow(calc_density(pdf), cmap='jet')
+
+def animate(i):
+
+    global pdf
+    omega = 1
+    pdf_streamed = streaming(pdf)
+
+    assert np.allclose(np.sum(pdf), np.sum(pdf_streamed), atol=1e-3) # check mass conservation
+    
+    # recalculate the density
+    density = calc_density(pdf_streamed)
+
+    #print("density = ", density)
+    #print("density shape = ", density.shape)
+    #print("density average = ", np.average(density))
+    # calculate local average velocity
+    local_avg_velocity = calc_local_avg_velocity(pdf_streamed)
+    """
+    print("local_avg_velocity = ", local_avg_velocity)
+    print("local_avg_velocity shape = ", local_avg_velocity.shape)
+    """
+    # calculate equilibrium pdf
+    equilibrium_pdf = calc_equilibrium_pdf(density, local_avg_velocity)
+    """
+    print("equilibrium_pdf = ", equilibrium_pdf)
+    print("equilibrium_pdf shape = ", equilibrium_pdf.shape)
+    """
+    
+    # collision step using np.roll
+    pdf_collision = pdf_streamed + omega*(equilibrium_pdf - pdf_streamed)
+    """
+    print("pdf_collision = ", pdf_collision)
+    print("pdf_collision shape = ", pdf_collision.shape)
+    """
+
+
+    assert np.allclose(np.sum(pdf_streamed), np.sum(pdf_collision), atol=1e-3 )# check mass conservation
+
+    pdf = pdf_collision
+    # update the image
+    im.set_array(calc_density(pdf))
+    return im,
+
+anim = animation.FuncAnimation(fig, animate, frames=300, interval=200, blit=True)
+#HTML(anim.to_html5_video())
+plt.show()
